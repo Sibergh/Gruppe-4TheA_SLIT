2d688e2a82a8b03a0c80e6868ccec29bb08408a2
Alexander Mitchell <AlexanderMitchellschool@gmail.com>
2014-10-06 12:36:41 +0200

    Tatt vekk tests.

--- Kildekode/mySQL/Ferdig (BETA)/slitDBv4.sql
+++ Kildekode/mySQL/Ferdig (BETA)/slitDBv4.sql
@@ -1,9 +1,5 @@
 create database slitV3;
 use slitv3;
-drop database slitv3;
-
-drop table users;
-
 
 create table users(
 userID int NOT NULL auto_increment,
@@ -17,13 +13,6 @@ constraint users_userID_pk primary key (userID),
 constraint users_ModNo_fk foreign key  (ModNo) references usermodule (ModNo)
 );
 
-insert into users (`fname`,`lname`,`email`,`phoneno`, `pw`)
-	values ('kristian','Nilsen','karl@gmail.com','87654321', 'lolawq1332');
-
-select * from users;
-
-drop table usermodule;
-
 create table usermodule(
 ModNo int NOT NULL auto_increment,
 status1 int(30),
@@ -34,11 +23,6 @@ Modcrit varchar(500),
 constraint ModNo_pk primary key (ModNo)
 );
 
-insert into usermodule(`ModDesc`,`ModGoal`,`Resources`,`ModCrit`)
-	values ('kaskask','1234rt','1987-06-01','karl@gmail.com');
-
-select * from usermodule;
-drop table state;
 
 /* verdier for status 1=godkjent, 2=ventende(pending), 3=Ikke godkjent */
 create table state(
@@ -49,19 +33,6 @@ constraint status1_pk primary key (status1),
 constraint state_userID_fk foreign key  (userID) references users (userID),
 constraint state_ModNo_fk foreign key (ModNo) references usermodule (ModNo)
 );
-
-insert into state(`status1`)
-	values ('1');
-
-insert into state(`status1`)
-	values ('2');
-
-insert into state(`status1`)
-	values ('3');
-
-select * from state;
-
-drop table course;
 
 create table course(
 courseID int auto_increment,
@@ -75,14 +46,6 @@ constraint course_userID_fk foreign key  (userID) references users(userID),
 constraint course_commenNo_fk foreign key  (ModNo) references usermodule (ModNo)
 );
 
-
-insert into course(`courseName`, `teacherName`, `courseDesc`)
-	values ('IKT', 'Ola Malnes', 'kaksakskas');
-
-select * from course;
-
-drop table comments;
-
 create table comments(
 commentDesc varchar(200),
 ModNo int(3),
@@ -91,8 +54,3 @@ constraint commentDesc_pk primary key (commentDesc),
 constraint comments_usermodule_fk foreign key  (ModNo) references usermodule (ModNo),
 constraint comments_userID_fk foreign key  (userID) references users (userID)
 );
-
-insert into comments(`commentDesc`)
-	values ('get bitchez fuck money');
-
-select * from comments;
